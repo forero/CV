852f6635db790570ce31a983c3bab3a92af0aa0c
forero <j.e.forero.romero@gmail.com>
2015-10-25 08:10:16 -0400

    simple stats

---
+++ Vita/uniandes_grade.py
@@ -0,0 +1,6 @@
+import numpy as np
+rating = np.array([3.41, 3.90, 3.52, 3.36, 3.47, 3.76, 3.55, 3.49, 3.78, 3.86, 3.65, 3.86])
+n_students = np.array([65, 10, 68, 28, 61, 12, 60, 20, 64, 10, 6, 96])
+
+print n_students.sum()
+print sum(rating * n_students)/sum(n_students)
